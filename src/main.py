14baae3181dabf6723152b0c3ad4422a92af8aa1
Thomas Hensel <thomas.hensel@mpinat.mpg.de>
2023-04-19 10:29:57 +0200

    remove test from main

--- src/main.py
+++ src/main.py
@@ -4,11 +4,8 @@ sys.path.insert(0, root_path)
 
 from package.analysis import *
 from package.transform import area_circ
-from package.test.test_analysis import test_create_data
 
 if __name__=='__main__':
-    # run some tests:
-    test_create_data()
 
     freq_list = [1, 2, 3]
     file_name = 'data.csv'
